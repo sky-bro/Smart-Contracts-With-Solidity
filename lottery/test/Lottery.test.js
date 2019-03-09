c8fc894816cc6b334850c8e69bf14922b6df24b3
sky <sky_bro@qq.com>
2019-03-09 11:28:34 +0800

    init lottery contract, test setup

---
+++ lottery/test/Lottery.test.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+
+// const provider = ganache.provider();
+// const web3 = new Web3(provider);
+const web3 = new Web3(ganache.provider());
+const {interface, bytecode} = require('../compile');
+
+let accounts;
+let lottery;
+
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+  // Use one of those accounts to deploy the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode})
+    .send({ from: accounts[0], gas: '1000000' });
+  // inbox.setProvider(provider);
+});
+
+describe('Lottery Contract', () => {
+  it('', async () => {
+
+  });
+});
