819aa41ba8f6ed7c28b1c1d083c853b6a57eb4f1
sky <sky_bro@qq.com>
2019-03-10 09:22:08 +0800

    contract instance, display basic states

---
+++ lottery-react/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import web3 from "./web3";
+import lottery from "./lottery";
+
+class App extends Component {
+  state = {
+    manager: "",
+    players: [],
+    balance: ""
+  };
+
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+    this.setState({ manager, players, balance });
+  }
+  render() {
+    // console.log(web3.version);
+    // web3.eth.getAccounts().then(console.log);
+    return (
+      <div className="App">
+        <h2>Lottery Contract </h2>
+        <p>
+          This contract is managed by {this.state.manager}. There are currently{" "}
+          {this.state.players.length} people entered, competing to win{" "}
+          {web3.utils.fromWei(this.state.balance, "ether")} ether!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
