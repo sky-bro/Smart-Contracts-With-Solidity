e6f565f552cfb7652ff3f67e5451b7b8b2abeefa
sky <sky_bro@qq.com>
2019-03-17 01:37:09 +0800

    fix version problems

--- lottery/deploy.js
+++ lottery/deploy.js
@@ -1,21 +1,22 @@
-const HDWalletProvider = require('truffle-hdwallet-provider');
-const Web3 = require('web3');
-const {interface, bytecode} = require('./compile');
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
 
 const provider = new HDWalletProvider(
-  'summer chunk wait rally honey flight provide clerk execute provide post about',
-  'https://rinkeby.infura.io/v3/f26eafb5e6ff4cdbaa04bd5140fee1d9'
+  "summer chunk wait rally honey flight provide clerk execute provide post about",
+  "https://rinkeby.infura.io/v3/f26eafb5e6ff4cdbaa04bd5140fee1d9"
 );
 
 const web3 = new Web3(provider);
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
-  console.log('Attempting to deploy from account', accounts[0]);
+  console.log("Attempting to deploy from account", accounts[0]);
   const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({data: bytecode})
-    .send({gas: '1000000', from: accounts[0]});
-  console.log('Contract deployed to', result.options.address);
+    .deploy({ data: bytecode })
+    .send({ gas: "1000000", from: accounts[0] });
+  console.log(interface);
+  console.log("Contract deployed to", result.options.address);
 };
 deploy();
 // provider.engine.stop();
