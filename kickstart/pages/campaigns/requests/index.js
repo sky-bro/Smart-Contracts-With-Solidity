b784f49e8b7847121c30f1c8ddd18b5ed2a65a09
sky <sky_bro@qq.com>
2019-03-28 09:51:37 +0800

    add create-new-requests page

--- kickstart/pages/campaigns/requests/index.js
+++ kickstart/pages/campaigns/requests/index.js
@@ -1,8 +1,26 @@
 import React, { Component } from "react";
+import Layout from "../../../components/Layout";
+import { Link } from "../../../routes";
+import { Button } from "semantic-ui-react";
+import campaign from "../../../ethereum/campaign";
 
 class RequestIndex extends Component {
+  static async getInitialProps(props) {
+    const { address } = props.query;
+    return { address, requestsLength };
+  }
   render() {
-    return <h3>Requests Index</h3>;
+    return (
+      <Layout>
+        <h3>Requests Index</h3>
+
+        <Link route={`/campaigns/${this.props.address}/requests/new`}>
+          <a>
+            <Button primary>Add request</Button>
+          </a>
+        </Link>
+      </Layout>
+    );
   }
 }
 
